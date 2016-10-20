4fffbec7148da4bac9b52493b02589a4f2650985
Matheus Ancelmo <Matheus Ancelmo@DESKTOP-RR7IH9N>
2016-10-20 16:32:40 -0200

    Novo commit para adicionar

---
+++ ProjetoSomaVideo/src/Soma.java
@@ -0,0 +1,18 @@
+
+public class Soma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int x = 5;
+		int y = 15;
+		int z = 0;
+		
+		z = x + y;
+		
+		System.out.println(z);
+		
+	}
+
+}
